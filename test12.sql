35e399b8aae0fe198a44d8fbc525c2b1ed804ff5
Amit <amitarwal88@gmail.com>
2025-12-29 16:55:14 +0530

    adding a test sql

---
+++ test12.sql
@@ -0,0 +1,8 @@
+merge this code
+-- This SQL script creates a sample table and inserts some data into it.
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    HireDate DATE
+);
