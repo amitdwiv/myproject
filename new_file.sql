3c6fe82e67d48e56c62246f23904ba86a8b92341
Amit <amitarwal88@gmail.com>
2025-12-30 20:32:07 +0530

    testing if new and exxisting code works together

---
+++ new_file.sql
@@ -0,0 +1,5 @@
+-- new code
+CREATE TABLE NewTable (
+    ID INT PRIMARY KEY,
+    Name VARCHAR(100)
+);
--- testfile.sql
+++ testfile.sql
@@ -1,1 +1,4 @@
 select *from dual;
+
+Adding new item;
+insert into mytable (id, name) values (1, 'New Item');
